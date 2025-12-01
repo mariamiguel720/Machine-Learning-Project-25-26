bc2f6e89f0f7ebb6fe643fa0009c57fabda4851d
mariamiguel720 <mariamfonseca2004@gmail.com>
2025-12-01 10:21:13 +0000

    Colocar funcções do modelling em . py

--- ENTREGA FINAL/notebooks/functions.py
+++ ENTREGA FINAL/notebooks/functions.py
@@ -344,3 +344,63 @@ def data_preparation(df_fit, df_to_apply, col_thresholds, ordinal_cols, one_hot_
 #def correct_values(train):
 
 
+
+
+
+
+
+
+
+# ----------------- MODEL AND ASSESSMENT ----------------- #
+
+def train_model(model, X_train, y_train):
+    """ Train the given model on provided data."""
+    model.fit(X_train, y_train) # Fit the model
+    return model
+
+
+
+def evaluate_model(model, X, y):
+    """Evaluate a regression model using multiple metrics:
+    R2, Adjusted R2, MAE, MSE, RMSE, MedAE, MAPE."""
+    n_samples = X.shape[0] # number of observations
+    n_features = X.shape[1] # number of features
+    
+    y_pred = model.predict(X) # predicted values
+    
+    # Calculate metrics
+    r2 = r2_score(y, y_pred)
+    adj_r2 = 1 - (1 - r2) * (n_samples - 1) / (n_samples - n_features - 1)
+    mae = mean_absolute_error(y, y_pred)
+    mse = mean_squared_error(y, y_pred)
+    rmse = rmse = np.sqrt(mse)
+    medae = median_absolute_error(y, y_pred)
+    mape = mean_absolute_percentage_error(y, y_pred)
+
+# Compile metrics into a dictionary
+    metrics_dict = {
+        'R2': r2,
+        'Adjusted R2': adj_r2,
+        'MAE': mae,
+        'MSE': mse,
+        'RMSE': rmse,
+        'MedAE': medae,
+        'MAPE (%)': mape
+    }
+    return metrics_dict
+
+
+
+# Create comparison DataFrame
+def comparison_metrics(model, X_train, y_train, X_val, y_val):
+    """Create a comparison DataFrame for training and validation metrics."""
+
+    metrics_train = evaluate_model(model, X_train, y_train)
+    metrics_val = evaluate_model(model, X_val, y_val)
+    
+    comparison_df = pd.DataFrame({
+        'Metric': list(metrics_train.keys()),
+        'Train': list(metrics_train.values()),
+        'Validation': list(metrics_val.values())
+    })
+    return comparison_df
